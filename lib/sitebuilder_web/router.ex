588d864b7f8bf81512775be5317733ddf089521a
Phidias Lo <aldenoah.zero@gmail.com>
2018-05-28 00:34:52 +0800

    Add jumbotron and links to nabber

--- lib/sitebuilder_web/router.ex
+++ lib/sitebuilder_web/router.ex
@@ -17,6 +17,9 @@ defmodule SitebuilderWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/about", PageController, :about
+    get "/contact", PageController, :contact
+    get "/privacy-policy", PageController, :privacy
   end
 
   # Other scopes may use custom stacks.
