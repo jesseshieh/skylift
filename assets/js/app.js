acbc1f6ee2120c87f52eb2de7d6e4c19d0295a80
Phidias Lo <aldenoah.zero@gmail.com>
2018-06-08 06:30:42 +0800

    Add vue clickaway

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,17 @@
 import "phoenix_html"
 import "../css/app.css"
 import Vue from "vue"
+import { mixin as clickaway } from 'vue-clickaway';
 
 new Vue({
   el: "#sitebuilder",
+  mixins: [ clickaway ],
   data: {
     menu: false
   },
+  methods: {
+    closeMenu: function() {
+      this.menu = false
+    }
+  }
 })
